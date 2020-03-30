bc134fc905fbde631f2afb9c6218ba06c9357c9e
jakobln <62882745+jakobln@users.noreply.github.com>
2020-03-30 13:45:15 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RolePlayV10
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            Warrior warriorA = new Warrior("Ragnar");
+            Warrior warriorB = new Warrior("Lagertha");
+
+            Console.WriteLine($"Warrior A is called {warriorA.Name}");
+            Console.WriteLine($"Warrior B is called {warriorB.Name}");
+        
+            Console.WriteLine($"Warrior A's level is {warriorA.level}");
+            Console.WriteLine($"Warrior B's level is {warriorB.level}");
+
+            Console.WriteLine($"Warrior A has leveled up and is now level {warriorA.levelup}");
+            Console.WriteLine($"Warrior B has leveled up and is now level {warriorB.levelup}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
---
+++ Warrior.cs
@@ -0,0 +1,31 @@
+﻿namespace RolePlayV10
+{
+    public class Warrior
+    {
+        private string _name;
+        private int _level;
+        private int _levelup;
+
+        public Warrior(string name)
+        {
+            _name = name;
+            _level = 1;
+            _levelup = _level + 1;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int level
+        {
+            get { return _level; }
+        }
+
+        public int levelup
+        {
+            get { return _levelup; }
+        }
+    }
+}
